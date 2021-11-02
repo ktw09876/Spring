1fdc5c28e37d9cc923540773d8a6e723cbfae666
dhqhf <dhqhf@59.3.42.74>
2021-11-02 13:12:36 +0900

    스프링 DI, AOP

---
+++ spring/src/spring/di/entity/NewlecExam.java
@@ -0,0 +1,85 @@
+package spring.di.entity;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+//@Component //객체화할 때 사용, @Service, Controller
+public class NewlecExam implements Exam {
+	
+	@Value("20") //값 초기화
+	private int kor;
+	@Value("30")
+	private int eng;
+	private int math;
+	private int com;
+	
+	public NewlecExam() {
+		// TODO Auto-generated constructor stub
+	}	
+	
+	public NewlecExam(int kor, int eng, int math, int com) {
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		this.com = com;
+	}
+	
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+	public void setMath(int math) {
+		this.math = math;
+	}
+
+	public int getCom() {
+		return com;
+	}
+
+	public void setCom(int com) {
+		this.com = com;
+	}
+
+	@Override
+	public int total() {
+		// TODO Auto-generated method stub
+		return kor+eng+math+com;
+	}
+
+	@Override
+	public float avg() {
+		// TODO Auto-generated method stub
+		return total()/4.0f;
+	}
+
+	@Override
+	public String toString() {
+		return "NewlecExam [kor=" + kor + ", eng=" + eng + ", math=" + math + ", com=" + com + "]";
+	}
+
+	@Override
+	public void add(NewlecExam newlecExam) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
